ebbdfc0fa6c0ebd21f5129cf5f07ba8771405aef
AkiOffice73 <akito7273@gmail.com>
2017-12-15 18:41:56 +0800

    init

---
+++ KiraraSaveDataManeger/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace KiraraSaveDataManeger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(432, 202);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(432, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(884, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chg Name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "涼風青葉(風魔) Aoba",
+            "千矢(炎騎) Chiya",
+            "九条カレン(月戰) Karen",
+            "本田珠輝(土魔) Tamaki",
+            "透 -- トオル(水戰) Tooru ",
+            "由紀(水僧) Yuki",
+            "野々原ゆずこ(炎戰) Yuzuko",
+            "由乃 -- ゆの(陽僧) Yuno "});
+            this.listBox1.Location = new System.Drawing.Point(432, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(446, 132);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "select dir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 7);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(600, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "C:\\Users\\USER\\Downloads\\KIRARA";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(16, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(410, 692);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(699, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 21);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "上層";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 757);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ KiraraSaveDataManeger/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KiraraSaveDataManeger
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo SaveDir;
+        DirectoryInfo[] SaveDirArray;
+        DirectoryInfo SelectedDir;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2_Click(null, null);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveDir = new DirectoryInfo(textBox2.Text);
+            SaveDirArray = SaveDir.GetDirectories();
+            //SaveDirs = Directory.GetDirectories(textBox2.Text);
+            int tmpScroll = listBox2.SelectedIndex;
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(SaveDirArray);
+            if (tmpScroll < listBox2.Items.Count)
+            {
+                listBox2.SelectedIndex = tmpScroll;
+            }
+            listBox2.Focus();
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = listBox2.SelectedIndex;
+            if (index != -1 && index < listBox2.Items.Count)
+            {
+                SelectedDir = SaveDirArray[index];
+                if (SelectedDir.Exists == false)
+                    return;
+
+                textBox1.Text = SelectedDir.Name;
+                
+                var png = SelectedDir.GetFiles("*.png");
+                if (png.Length > 0)
+                {
+                    var tmpImg = Image.FromFile(png[0].FullName);
+                    pictureBox1.Image = new Bitmap(tmpImg);
+                    tmpImg.Dispose();
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            if(SelectedDir != null && SelectedDir.Exists)
+            {
+                //MessageBox.Show(SelectedDir.Parent.FullName);
+                var newPath = SelectedDir.Parent.FullName + "\\" + textBox1.Text;
+                if (!Directory.Exists(newPath))
+                {
+                    SelectedDir.MoveTo(newPath);
+                    button2_Click(null, null);
+                    button1.Text = rnd.Next(100000).ToString();
+                }
+                else
+                {
+                    button1.Text = "資料夾已存在";
+                }
+            }
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            var index = listBox2.SelectedIndex;
+            if (index != -1 && index < listBox2.Items.Count)
+            {
+                SelectedDir = SaveDirArray[index];
+                textBox2.Text = SelectedDir.FullName;
+                button2_Click(null, null);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = SaveDir.Parent.FullName;
+            button2_Click(null, null);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            var sleGoldIndex = listBox1.SelectedIndex;
+            string[] GoldName = { "_Aoba", "_Chiya", "_Karen", "_Tamaki", "_Tooru", "_Yuki", "_Yuzuko", "_Yuno" };
+            var tmpStr = textBox1.Text;
+            textBox1.Text = tmpStr.Substring(0, 1) + GoldName[sleGoldIndex] + tmpStr.Substring(1);
+
+        }
+    }
+}
